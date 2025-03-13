af5f03e5705d8df5548e6777181787e9a15d1132
TarekAmmar1993 <tarak.ammar@hotmail.com>
2025-03-13 11:27:44 +0100

    refactor: migrate from create-react-app to nextjs app

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 ```bash
 git clone https://github.com/TarekAmmar1993/memoryCards.git
-npm install
-npm start
+pnpm install
+pnpm run dev
 ```
-## Deployment 
+
+## Deployment
+
 App: https://tarekammar1993.github.io/memoryCards-deployed/
--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Header from './components/Header';
 import Cards from './components/Cards';
+import { JobPrepContextProvider } from './context/context';
+import React from 'react';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Cards />
-    </div>
+    <JobPrepContextProvider>
+      <React.StrictMode>
+        <div className="app">
+          <Header />
+          <Cards />
+        </div>
+      </React.StrictMode>
+    </JobPrepContextProvider>
   );
 }
 
