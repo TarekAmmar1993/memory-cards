40e05eaae054f0507b2261b73521aeb5f9319202
TarekAmmar1993 <tarak.ammar@hotmail.com>
2025-03-17 14:25:24 +0100

    add shadcn; apply styling header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import Modal from './Modal';
 import QuestionForm from './QuestionForm';
-
+import { SquarePlus } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 function Header() {
   const [showModal, setShowModal] = useState(false);
 
@@ -10,17 +11,18 @@ function Header() {
   }
 
   return (
-    <div className="header" style={{ float: 'left' }}>
-      <span>
-        <button className="add-btn" onClick={handleAddButton}>
-          <span className="text"> Add Card +</span>
-        </button>
-      </span>
+    <div className="bg-linear-to-r/hsl from-indigo-700 to-indigo-400 flex justify-between items-center p-3">
+      <h1 className="font-bold text-amber-400"> Memory Cards </h1>
+      <Button
+        onClick={handleAddButton}
+        className="bg-amber-400 cursor-pointer hover:bg-amber-300"
+      >
+        <SquarePlus /> Add Card
+      </Button>
+
       <Modal showModal={showModal} setShowModal={setShowModal}>
         <QuestionForm setShowModal={setShowModal} />
       </Modal>
-
-      <span style={{ marginLeft: '35vw' }}> Memory Cards </span>
     </div>
   );
 }
