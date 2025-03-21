ed5aa7f11604db0e37b83b81bd7c4b2058ab37dc
TarekAmmar1993 <tarak.ammar@hotmail.com>
2025-03-21 15:33:18 +0100

    feat: add hover effect for cta buttons

--- src/components/CardCTA.tsx
+++ src/components/CardCTA.tsx
@@ -4,10 +4,16 @@ import { ArrowClockwise, Trash, X } from 'react-bootstrap-icons';
 const CardCTA = ({ handleExit, handleDelete, handleFlip }) => {
   return (
     <div>
-      <div className="fixed top-4 right-4 cursor-pointer" onClick={handleExit}>
+      <div
+        className="fixed top-4 right-4 cursor-pointer hover:text-red-600"
+        onClick={handleExit}
+      >
         <X size="30px" />
       </div>
-      <div className="fixed top-4 left-4 cursor-pointer" onClick={handleFlip}>
+      <div
+        className="fixed top-4 left-4 cursor-pointer hover:animate-spin"
+        onClick={handleFlip}
+      >
         <ArrowClockwise size="30px" />
       </div>
       <div
