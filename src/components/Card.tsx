e2f45f5e9100c4c2c809be9c9c59b01a5b43aa11
TarekAmmar1993 <tarak.ammar@hotmail.com>
2025-04-22 18:17:15 +0100

    feat: add badges to cards statically

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
-import Modal from './Modal';
-import ZoomedCard from './ZoomedCard';
-import { ICard } from '../common/types';
-import Image from 'next/image';
-import dummyImg from '../assets/cp.jpg';
+import { useState } from "react";
+import Modal from "./Modal";
+import ZoomedCard from "./ZoomedCard";
+import { ICard } from "../common/types";
+import Image from "next/image";
+import dummyImg from "../assets/cp.jpg";
+import { Badge } from "@/components/ui/badge";
 
 const Card = ({ id, questionPreview, question, answer }: ICard) => {
   // State
@@ -15,14 +16,23 @@ const Card = ({ id, questionPreview, question, answer }: ICard) => {
 
   return (
     <div
-      className="rounded-lg shadow-2xl p-4 cursor-pointer w-72 border-4   odd:bg-[#EC781D] even:bg-[#43A3DB] font-default-color"
+      className="font-default-color flex w-72 cursor-pointer flex-col gap-3 rounded-lg border-4 p-4 shadow-2xl odd:bg-[#EC781D] even:bg-[#43A3DB]"
       onClick={handleClick}
     >
-      <div className="rounded-lg overflow-hidden mb-6 w-full ">
+      <div className="mb-6 w-full overflow-hidden rounded-lg">
         <Image src={dummyImg} alt="dummy image" />
       </div>
       <h2 className="font-medium"> Question </h2>
       <p> {questionPreview}</p>
+      <div className="mt-auto flex gap-2">
+        <Badge variant="outline" className="bg-amber-300">
+          React
+        </Badge>
+        <Badge variant="outline" className="bg-orange-600">
+          Angular
+        </Badge>
+      </div>
+
       <Modal showModal={showModal} setShowModal={setShowModal}>
         <ZoomedCard
           id={id}
